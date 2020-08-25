d9d0e80bbc9b301b5789de5019b8cf707d5fc91e
Gokoro <raycle1004@naver.com>
2020-08-26 01:33:35 +0900

    Add translation files
    - Add English
    - Add Korean
    - Add `romaji` to be set 'English' by default
    - Add japanese only file but not an article

---
+++ public/locale/english.js
@@ -0,0 +1,68 @@
+export default {
+    // Page Section
+    main: {
+        nextUp: "Up Next",
+        todayUp: "Today's Anime",
+        seeDetails: "See details"
+    },
+    day: {
+        headers: {
+            sun: "Sun's Anime",
+            mon: "Mon's Anime",
+            tue: "Tue's Anime",
+            wed: "Wed's Anime",
+            thu: "Thu's Anime",
+            fri: "Fri's Anime",
+            sat: "Sat's Anime",
+            today: 'Today'
+        }
+    },
+    about: {
+        repository: 'Repository',
+        contact: 'Contact',
+        contactDiscord: 'Ohys-Raws Discord',
+        copyright: 'Copyright',
+        copyArticle: 'Files are provided by Ohys-Raws.'
+    },
+    // Component Section
+    components: {
+        header: {
+            now: 'Now'
+        },
+        sidebar: {
+            ohysFamily: 'Ohys Family',
+            contact: 'Contact',
+            language: 'Language'
+        },
+
+        // At the main page commonly
+        animeRemainTimeSection: {
+            isAir: "On Air",
+            soonAir: "Will air in",
+        },
+
+        // At the details page
+        animeTorrentList: {
+            torrentList: 'Torrent List'
+        },
+        animeDescriptionSection: {
+            story: 'Story',
+            more: 'More'
+        },
+        animeTorrentOtherLink: {
+            externalLinks: 'External Links'
+        }
+    },
+    // Common Section
+    common: {
+        day: {
+            sun: 'Sun',
+            mon: 'Mon',
+            tue: 'Tue',
+            wed: 'Wed',
+            thu: 'Thu',
+            fri: 'Fri',
+            sat: 'Sat'
+        }
+    }
+}
---
+++ public/locale/japanese.js
@@ -0,0 +1,68 @@
+export default {
+    // Page Section
+    main: {
+        nextUp: "",
+        todayUp: "",
+        seeDetails: ""
+    },
+    day: {
+        headers: {
+            sun: "",
+            mon: "",
+            tue: "",
+            wed: "",
+            thu: "",
+            fri: "",
+            sat: "",
+            today: ''
+        }
+    },
+    about: {
+        repository: '',
+        contact: '',
+        contactDiscord: '',
+        copyright: '',
+        copyArticle: ''
+    },
+    // Component Section
+    components: {
+        header: {
+            now: ''
+        },
+        sidebar: {
+            ohysFamily: '',
+            contact: '',
+            language: ''
+        },
+
+        // At the main page commonly
+        animeRemainTimeSection: {
+            isAir: "",
+            soonAir: "",
+        },
+
+        // At the details page
+        animeTorrentList: {
+            torrentList: ''
+        },
+        animeDescriptionSection: {
+            story: '',
+            more: ''
+        },
+        animeTorrentOtherLink: {
+            externalLinks: ''
+        }
+    },
+    // Common Section
+    common: {
+        day: {
+            sun: '',
+            mon: '',
+            tue: '',
+            wed: '',
+            thu: '',
+            fri: '',
+            sat: ''
+        }
+    }
+}
