9758affed5b90b713e01391edb68450e2c7314c4
Gokoro <lincroe@gmail.com>
2020-11-22 21:20:06 +0900

    Turn the button that changes between days into router.push()

--- src/components/ChangeDayButton.js
+++ src/components/ChangeDayButton.js
@@ -1,35 +1,43 @@
-import Link from 'next/link'
+import Router, { useRouter } from 'next/router'
+
 import ColoredButton from './ColoredButton'
 
 const ChangeDayButton = props => {
+    const router = useRouter()
     const isPrev = props.dayType === 'prev'
 
-    return (
-        <Link href="/[day]" as={`/${props.day}`}>
-            <a className="item">
-                <ColoredButton bgColor="#1abc9c" hoverBgColor="#16a085" textColor="#FFFFFF" >
-                    <div className="container">
-                        <img className="arrow" src="/svg/arrow-right-white-icon.svg"/>
-                        <span className="text">
-                            {isPrev ? 'Prev' : 'Next'}
-                        </span>
-                        <style jsx>{`
-                            .container {
-                                display: flex;
-                                flex-direction: ${isPrev ? 'row' : 'row-reverse'}
-                            }
-                            .arrow {
-                                ${isPrev && 'transform: rotate(180deg)'}
-                            }
-                            .text {
-                                margin-${isPrev ? 'left' : 'right'}: 6px;
-                                color: #FFFFFF !important;
-                            }
-                        `}</style>
-                    </div>
-                </ColoredButton>
-            </a>
-        </Link>
-    )
+    const onClick = () => {
+        router.push(`/${props.day}`)
+        Router.events.on('routeChangeComplete', () => scrollTo(0, 0))
+    }
+
+    return (<>
+        <span className="routerButton" onClick={onClick}>
+            <ColoredButton bgColor="#1abc9c" hoverBgColor="#16a085" textColor="#FFFFFF" >
+                <div className="container">
+                    <img className="arrow" src="/svg/arrow-right-white-icon.svg"/>
+                    <span className="text">
+                        {isPrev ? 'Prev' : 'Next'}
+                    </span>
+                </div>
+            </ColoredButton>
+        </span>
+        <style jsx>{`
+            .routerButton {
+                cursor: pointer;
+            }
+            .container {
+                display: flex;
+                flex-direction: ${isPrev ? 'row' : 'row-reverse'}
+            }
+            .arrow {
+                ${isPrev && 'transform: rotate(180deg)'}
+            }
+            .text {
+                margin-${isPrev ? 'left' : 'right'}: 6px;
+                color: #FFFFFF !important;
+            }
+        `}</style>
+    </>)
 }
 export default ChangeDayButton
